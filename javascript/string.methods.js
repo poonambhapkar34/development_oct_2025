e7c6d280a122e6e29018d8b55a70962a041ee577
poonambhapkar34 <poonambhapkar34@gmail.com>
2026-01-31 12:56:15 +0530

    String Methods 1-31-2026

--- javascript/string.methods.js
+++ javascript/string.methods.js
@@ -11,8 +11,24 @@ console.log( text2.concat( text12));
 //slice() extracts a part of a string and returns the extracted part in a new string.
 //The method takes 2 parameters: start position, and end position (end not included).
 
-let text4 = "Apple, Banana, Kiwi";
+let text4 = "Apple,kBanana, Kiwi";
 console.log(text4.slice(7, 13)); //(si, ei) includes start ind, excludes end ind
 console.log(text4.slice(7,)); 
 console.log(text4.slice(13));
 console.log(text4.slice(-12, -7)); // (End i, start I) //
+console.log(text4.substring(-12, 7)); //(0,0) //It will consider negative indexes as a 0
+Fname = "   poonam    ";
+LName= "bhapkar"
+FullName = Fname.concat(LName)//Fname + LName
+console.log(text4.substr(7,6)); // (SI , len)
+console.log(Fname.trim());
+
+num = 10;
+console.log(typeof(num));
+console.log(typeof(num.toString()));
+
+let Ntext = "poonam | patil | bhapkar";
+console.log(Ntext.split("|")); //Split will return array // object
+
+let text3 = "Hello world, welcome to the universe.";
+console.log(text3.toLocaleLowerCase().includes("world"));
