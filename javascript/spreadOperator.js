281f12556a759766259677850cbf8447aa3a7510
poonambhapkar34 <poonambhapkar34@gmail.com>
2026-02-15 13:45:22 +0530

    array ittration methods

--- javascript/spreadOperator.js
+++ javascript/spreadOperator.js
@@ -25,5 +25,24 @@ const car_more = {
 let newCar = {...car_more, ...car};
 console.log(newCar);
 
+//flat:
+const myArr = [[1,2],[3,4],[5,6]];
+
+const newArr = myArr.flat();
+console.log(newArr);
+
+//The reduce() Method
+//Find the sum of all numbers in an array:
+const numbers = [45, 54, 99, 26, 25];
+//console.log(numbers.reduce());
 
 
+const numbers1 = [45, 54, 99, 26, 25,4];
+let res = numbers1.every((item) => item > 18); 
+
+console.log(res);
+
+const numbers2 = [5, 4, 9, 6, 5,4];
+let res1 = numbers2.some((item) => item > 18); 
+
+console.log(res1);
